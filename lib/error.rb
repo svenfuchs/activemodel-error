bb17df69e1d559ea827f0df0b83a79e6777ce86c
Sven Fuchs <svenfuchs@artweb-design.de>
2010-01-25 18:26:35 +0100

    maintain Message's initializer signature in Error

--- lib/error.rb
+++ lib/error.rb
@@ -3,15 +3,13 @@ require 'message'
 class Error < Message
   attr_reader :model, :attribute
 
-  def initialize(type, model, attribute, message = nil, values = {}, options = {})
-    @model, @attribute = model, attribute
-    values.update(:model => model, :attribute => attribute)
-    options.update(:scope => scope)
-    super(type, message || type, values, options)
+  def initialize(type, message = nil, values = {}, options = {})
+    @model, @attribute = values.values_at(:model, :attribute)
+    super(type, message || type, values, options.merge(:scope => scope))
   end
 
-  def to_s(format = :short)
-    super(format)
+  def to_s(variant = :short)
+    super
   end
 
   protected
